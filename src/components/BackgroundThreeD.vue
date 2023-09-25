6ffb22ad549bbe0f7ddd9fff584123c34a7553f9
Jaedon Heger <jaedon.heger@labs.epiuse.com>
2023-09-25 20:48:56 +0200

    Move config to seperate functions
    - move config and setup to seperate functions
    
    Todo:
    - add config to props
    
    Issue: #1

--- src/components/BackgroundThreeD.vue
+++ src/components/BackgroundThreeD.vue
@@ -23,38 +23,61 @@ export default {
 
   methods: {
     init() {
-      const container = this.$refs.container;
+      this.createScene();
+      this.createCamera();
+      this.createLighting();
+      this.loadGeometry();
+      this.createRenderer();
+    },
 
+    createScene() {
       this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color(0x444488);
+    },
+
+    createCamera() {
       this.camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
       );
-
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
-      this.mesh = cube;
-      this.scene.add(cube);
       this.camera.position.z = 5;
+    },
 
+    createRenderer() {
+      const container = this.$refs.container;
       this.renderer = new THREE.WebGLRenderer();
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       container.appendChild(this.renderer.domElement);
-      this.animate();
+    },
+
+    createLighting() {
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+      this.scene.add(directionalLight);
+    },
+
+    loadGeometry() {
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshToonMaterial({
+        color: 0x00ff00,
+      });
+      const cube = new THREE.Mesh(geometry, material);
+      this.mesh = cube;
+      this.scene.add(cube);
     },
 
     animate() {
       requestAnimationFrame(this.animate);
       this.mesh.rotation.z += 0.01;
+      this.mesh.rotation.x += 0.01;
       this.renderer.render(this.scene, this.camera);
     },
   },
 
   mounted() {
     this.init();
+    this.animate();
   },
 };
 </script>
